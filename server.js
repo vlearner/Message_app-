5d25fa9134e5c7fb3c54c25323b7ab634e34c697
vlearner <barotvishal12@outlook.com>
2018-08-20 10:25:33 -0500

    Update mongoDb connection

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mongoose = require('mongoose');
-
+//Database connection
+var config = require('/Users/vishalbarot/Desktop/Node.js/config/config')
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 // Need urlencoded for displaying message on front side
 app.use(bodyParser.urlencoded({extended: false}))
 
-var dbUrl = 'mongodb://admin:vedzam-cufRaz-9zojdu@ds125862.mlab.com:25862/message_app_node_';
 
 var messages = [
     {name: 'Tim', message: 'Hi'},
